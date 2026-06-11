8b0b353292466b3c1d1cc58b32c0cab921a6fa93
pandier <62116820+pandier@users.noreply.github.com>
2026-06-11 06:39:00 +0100

    fix: Do not include the body of unknown errors

--- crates/godot/src/client/mod.rs
+++ crates/godot/src/client/mod.rs
@@ -465,7 +465,7 @@ impl ParseResponse for surf::Response {
         } else if let Ok(error) = serde_json::from_str::<dto::Error>(&body) {
             Err(SrClientError::new(&error.error, &error.message))
         } else {
-            Err(SrClientError::unknown(&format!("{}: {}", self.status(), body)))
+            Err(SrClientError::unknown(&format!("Something went wrong ({})", self.status())))
         }
     }
 }
